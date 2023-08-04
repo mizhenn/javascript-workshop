0e4255a363eee2665e867a452c0ba68926e1dbe1
Mizhenn <henmizhquiri@hotmail.com>
2023-08-03 21:05:50 -0400

    updated JS

--- block15/object.js
+++ block15/object.js
@@ -1,3 +1,4 @@
+/* Create the customer object with initial key-value pairs */
 const customer = {
   firstName: "jake",
   lastName: "smith",
@@ -9,3 +10,22 @@ const customer = {
   favoriteStore: "Target",
   firstVisit: false,
 };
+
+console.log(customer);
+
+/* Correcting the mistakes with bracket notation */
+customer["email"] = "Jak3Smith1992@email.com";
+customer["phone"] = 3195551234;
+customer["zipCode"] = "63132";
+customer["favoriteFlavors"] = ["coffee", "strawberry", "matcha"];
+
+/* Removeing unwanted properties */
+delete customer.zipCode;
+delete customer.favoriteStore;
+
+/* Adding new properties with dot notation */
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+customer.futureFlavors = "mango";
+customer.todaysPurchaseCost = 5.32;
+
+console.log(customer);
