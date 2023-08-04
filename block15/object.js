3c17190847edae770f4f284696a783865d915294
Mizhenn <henmizhquiri@hotmail.com>
2023-08-03 20:55:08 -0400

    workshop-block15

---
+++ block15/object.js
@@ -0,0 +1,11 @@
+const customer = {
+  firstName: "jake",
+  lastName: "smith",
+  email: "jaekSmih!aol.com",
+  phone: undefined,
+  zipCode: "631",
+  favoriteFlavors: 32,
+  cupSize: "medium",
+  favoriteStore: "Target",
+  firstVisit: false,
+};
