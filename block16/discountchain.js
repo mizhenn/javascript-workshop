861a18dcc907c7a56828f77be83abe829593be06
Mizhenn <henmizhquiri@hotmail.com>
2023-08-09 13:42:01 -0400

    added new code based on my resources

--- block16/discountchain.js
+++ block16/discountchain.js
@@ -1,79 +1,62 @@
-// Workshop : Discount Chain.
 const timmy = {
-    prescription: "acetaminophen",
-    pricePerRefill: 25,
-    refills: 3,
-    subscription: false,
-    coupon: true,
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
 };
 
 const sarah = {
-    prescription: "diphenhydramine",
-    pricePerRefill: 50,
-    refills: 1,
-    subscription: true,
-    coupon: false,
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
 }
 
 const rocky = {
-    prescription: "phenylephrine",
-    pricePerRefill: 30,
-    refills: 5,
-    subscription: true,
-    coupon: true,
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
 }
 
-
-//refill total  $ per for customer
-function calculateRefillTotal(customer) {
+// Total amount for each customer
+function calculateTotalAmount(customer) {
   return customer.pricePerRefill * customer.refills;
 }
-console.log(calculateRefillTotal(rocky));
-console.log(calculateRefillTotal(sarah));
-console.log(calculateRefillTotal(timmy));
 
-
-// 25% discount if customer has subscription 
-function applySubscriptionDiscount(refillTotal, hasSubscription) {
-  if (hasSubscription)  {
-      return refillTotal * 0.75; // 25% discount....
+// subscription discount applying
+function applySubscriptionDiscount(totalAmount, customer) {
+  if (customer.subscription) {
+      return totalAmount * 0.75; // 25% discount apply
   }
-  return ( sarah.subscription, calculateRefillTotal(sarah) * 0.75);
+  return totalAmount; // No discount
 }
-//printing out
-console.log('Sarah => Your grand total is: ',applySubscriptionDiscount(sarah));
 
-
-//$10 dollars coupon if the customer has
-function applyCouponDiscount(refillTotal, hasCoupon) {
-  if (hasCoupon) {
-    return refillTotal - 10; //-$10 coupon....
+// coupon discount applying
+function applyCouponDiscount(totalAmount, customer) {
+  if (customer.coupon) {
+      return totalAmount - 10; // -$10 coupon apply
   }
-  return (timmy.coupon, calculateRefillTotal(timmy) - 10);
+  return totalAmount; // No discount
 }
-//printing out
-console.log("Timmy => Your grand total is: " , applyCouponDiscount(timmy));
 
+// test for customer Timmy has coupon
+const timmyTotalAmount = calculateTotalAmount(timmy);
+const timmyWithCouponDiscount = applyCouponDiscount(timmyTotalAmount, timmy); 
 
+// test for customer Sarah has subscription
+const sarahTotalAmount = calculateTotalAmount(sarah);
+const sarahWithSubscriptionDiscount = applySubscriptionDiscount(sarahTotalAmount, sarah);
 
+// test for customer rocky who has coupon and subscription
+const rockyTotalAmount = calculateTotalAmount(rocky);
+const rockyWithSubscriptionDiscount = applySubscriptionDiscount(rockyTotalAmount, rocky);  
+const rockyWithBothDiscounts = applyCouponDiscount(rockyWithSubscriptionDiscount, rocky);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// print and loggin for each customer
+console.log("Sarah => Your grand total is: ", sarahWithSubscriptionDiscount); // 37.5
+console.log("Timmy => Your grand total is: ", timmyWithCouponDiscount); // 65  
+console.log("Rocky => Your grand total is: ", rockyWithBothDiscounts); // 102.5
